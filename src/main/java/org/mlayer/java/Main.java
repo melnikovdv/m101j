bdd2201f5582fae1bf7c487987349a15c5c7b979
mdv <melnikovdv@gmail.com>
2015-03-18 15:28:19 +0300

    Hellow world with Spark and Freemarker template eng

--- src/main/java/org/mlayer/java/Main.java
+++ src/main/java/org/mlayer/java/Main.java
@@ -1,17 +1,38 @@
 package org.mlayer.java;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 import spark.Spark;
 
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
+        final Configuration configuration = new Configuration();
+        configuration.setClassForTemplateLoading(Main.class, "/");
+
         Spark.get(new Route("/") {
             @Override
             public Object handle(Request request, Response response) {
-                return "hw";
+                StringWriter sw = new StringWriter();
+                try {
+                    Template template = configuration.getTemplate("hello.ftl");
+
+                    Map<String, Object> helloMap = new HashMap<String, Object>();
+                    helloMap.put("name", "web app demo");
+
+                    template.process(helloMap, sw);
+                } catch (Exception e) {
+                    halt(500);
+                    e.printStackTrace();
+                }
+                return sw;
             }
         });
     }
---
+++ src/main/resources/hello.ftl
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>m101j test</title>
+</head>
+
+<body>
+    <h1>Hello ${name}</h1>
+</body>
+</html>
